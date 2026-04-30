2f84a1f96b91c5d486b050f5605dd375ad657336
Amit Bhujbal <22439139+amitbhujbal@users.noreply.github.com>
2026-04-30 20:59:15 +0530

    Update SHA256 checksum for sysinfo formula

--- formula/sysinfo.rb
+++ formula/sysinfo.rb
@@ -2,7 +2,7 @@ class Sysinfo < Formula
   desc "Simple CLI to show system information"
   homepage "https://github.com/amitbhujbal/sysinfo"
   url "https://github.com/amitbhujbal/sysinfo/archive/refs/tags/v1.1.0.tar.gz"
-  sha256 "381be97858434e10a446b369b99063d777a6330789ef075be1485f69d7698fc1"
+  sha256 "f46e847974fcda94c25b7599a9f3cafaebdab5b5f174f73c62bd83e060695aeb"
   license "MIT"
 
   depends_on "python@3.13"
