caf83a542398ca38609aa86b6b50908570468531
Amit Bhujbal <22439139+amitbhujbal@users.noreply.github.com>
2026-04-30 21:23:20 +0530

    Update sysinfo.rb

--- formula/sysinfo.rb
+++ formula/sysinfo.rb
@@ -2,7 +2,7 @@ class Sysinfo < Formula
   desc "Simple CLI to show system information"
   homepage "https://github.com/amitbhujbal/sysinfo"
   url "https://github.com/amitbhujbal/sysinfo/archive/refs/tags/v1.1.0.tar.gz"
-  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+  sha256 "a8e9b5f0fb1583f1131705b6d7cf89f9705425beb1e5340c0b796d9ccd595a76"
   license "MIT"
 
   depends_on "python@3.13"
